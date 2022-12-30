b185fcddcb61b351ee8ed08d2570a34b2a2726c4
David Bennett <dabennett10@gmail.com>
2022-12-30 11:58:28 -0500

    Initial Commit

---
+++ SimpleDemo/SimpleDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleDemoApp.swift
+//  SimpleDemo
+//
+//  Created by David Bennett on 11/3/22.
+//
+
+import SwiftUI
+
+@main
+struct SimpleDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
